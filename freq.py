1049e66df1cd8088641b35d39a9be69d70038744
Nathzxccode <you@example.com>
2023-04-01 14:34:12 +0100

    added thingy

--- freq.py
+++ freq.py
@@ -9,13 +9,13 @@ p = pyaudio.PyAudio()
 def s():
     sinewave = SineWave(pitch=0, pitch_per_second=10)
     sinewave.play()
-    sinewave.set_volume(0)
+    
 
     for i in range(12):
-
+        sinewave.set_volume(0)
         sinewave.set_pitch(i)
 
-        time.sleep(0.1)
+        time.sleep(0.3)
 
 
 if __name__ == "__main__":

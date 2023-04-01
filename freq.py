a8ecb1a25008354437cf0864e5a143d1f9a8e3f1
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-04-01 14:35:48 +0100

    Update freq.py

--- freq.py
+++ freq.py
@@ -1,22 +1,11 @@
 import time
-
-import pyaudio
 from pysinewave import SineWave
-import pyaudio as p
 
-p = pyaudio.PyAudio()
+sinewave = SineWave(pitch=0, pitch_per_second=10)
+sinewave.play()
+sinewave.set_volume(0)
 
-def s():
-    sinewave = SineWave(pitch=0, pitch_per_second=10)
-    sinewave.play()
-    sinewave.set_volume(0)
+for i in range(12):
+    sinewave.set_pitch(i)
 
-    for i in range(12):
-
-        sinewave.set_pitch(i)
-
-        time.sleep(0.1)
-
-
-if __name__ == "__main__":
-    s()
+    time.sleep(0.1)

de1fa54babee8a65b5e6cd69080b636b48cd333d
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-04-02 05:49:59 +0100

    Update freq.py

--- freq.py
+++ freq.py
@@ -12,33 +12,33 @@ def freq_to_pitch(freq):
     return 12 * math.log2(freq / 440) + 9
 
 
-# CHORDS = {
-#     0   : [-10, -6, -4],
-#     0.1 : [-7, -4, -2],
-#     0.2 : [-5, -2, 1],
-#     0.3 : [-3, 1, 3],
-#     0.4 : [-1, 3, 5],
-#     0.5 : [0, 4, 7],
-#     0.6 : [2, 5, 9],
-#     0.7 : [3, 7, 10],
-#     0.8 : [5, 9, 12],
-#     0.9 : [7, 11, 14],
-#     1   : [9, 13, 16]
-# }
-
 CHORDS = {
-    0: [-6, -2, 1],
-    0.1: [-5, -1, 2],
-    0.2: [-4, 0, 3],
-    0.3: [-2, 2, 5],
-    0.4: [-1, 3, 6],
-    0.5: [0, 4, 7],
-    0.6: [1, 5, 8],
-    0.7: [2, 6, 9],
-    0.8: [4, 8, 11],
-    0.9: [5, 9, 12],
-    1: [6, 10, 13]
+    0   : [-10, -6, -4],
+    0.1 : [-7, -4, -2],
+    0.2 : [-5, -2, 1],
+    0.3 : [-3, 1, 3],
+    0.4 : [-1, 3, 5],
+    0.5 : [0, 4, 7],
+    0.6 : [2, 5, 9],
+    0.7 : [3, 7, 10],
+    0.8 : [5, 9, 12],
+    0.9 : [7, 11, 14],
+    1   : [9, 13, 16]
 }
+
+# CHORDS = {
+#     0: [-6, -2, 1],
+#     0.1: [-5, -1, 2],
+#     0.2: [-4, 0, 3],
+#     0.3: [-2, 2, 5],
+#     0.4: [-1, 3, 6],
+#     0.5: [0, 4, 7],
+#     0.6: [1, 5, 8, 12, 15],
+#     0.7: [2, 6, 9, 14, 18],
+#     0.8: [4, 8, 11, 16, 22],
+#     0.9: [5, 9, 12, 18, 24],
+#     1: [6, 10, 13, 20, 27]
+# }
 
 
 class Waves:
@@ -53,7 +53,7 @@ class Waves:
 
         new_chord = []
         for c in self.initial_pitches:
-            new_chord.append(c + (difference*2))
+            new_chord.append(c + (difference*10))
         print(new_chord)
         return new_chord
 
@@ -65,7 +65,7 @@ class Waves:
                 self.waves.append(SineWave(pitch=pitches[len(pitches)-len(self.waves)]))
 
         if len(pitches) < len(self.waves):
-            self.waves = self.waves[:len(pitches)]
+            self.waves = self.waves[1:len(pitches)-1]
 
         for i in range(len(self.waves)):
             self.waves[i].set_pitch(pitches[i])

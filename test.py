3b33587b0e5e69905d6062f57114cfb6d3385110
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2023-04-02 04:10:17 +0100

    added new .wavs

--- test.py
+++ test.py
@@ -1,27 +1,26 @@
-import wave
-from pydub import AudioSegment
+import pygame
+# Initialize Pygame
+pygame.mixer.init()
 
-# Load the two audio files
-audio_file1 = AudioSegment.from_wav("chord1.wav")
-audio_file2 = AudioSegment.from_wav("chord2.wav")
+# Set the number of mixer channels
+pygame.mixer.set_num_channels(3)
 
-# Define the duration of the crossfade in milliseconds
-crossfade_duration = 5000  # 5 seconds
+# Load the sound files
+sound1 = pygame.mixer.Sound('high-hat.wav')
+sound2 = pygame.mixer.Sound('bass.wav')
+sound3 = pygame.mixer.Sound('synth-chord.wav')
 
-# Get the last few seconds of audio from the first file
-last_seconds = audio_file1[-crossfade_duration:]
+# Define a function to play sounds based on input
+def play_sound(input):
+    if input == 1:
+        channel = pygame.mixer.Channel(0)
+        channel.play(sound1)
+    elif input == 2:
+        channel = pygame.mixer.Channel(1)
+        channel.play(sound2)
+    elif input == 3:
+        channel = pygame.mixer.Channel(2)
+        channel.play(sound3)
 
-# Create a new AudioSegment that is the crossfade between the two files
-crossfade = last_seconds.append(audio_file2, crossfade=crossfade_duration)
-
-# Export the crossfade to a new .wav file
-crossfade.export("crossfade.wav", format="wav")
-
-# Play the crossfade using the wave module
-with wave.open("crossfade.wav", "rb") as wf:
-    params = wf.getparams()
-    frames = wf.readframes(wf.getnframes())
-    wave_out = wave.open("default", "wb")
-    wave_out.setparams(params)
-    wave_out.writeframes(frames)
-    wave_out.close()
+if __name__ == "__main__":
+    play_sound(1)
